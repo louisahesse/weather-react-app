769d085460045a9b1a53bdd22b23bfc17e920720
louisahesse <louisahesse09@gmail.com>
2023-02-09 15:59:56 +0000

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export default function footer() {
+  return (
+    <div className="footer">
+      <a href="https://github.com/louisahesse/weather-react">
+        Open-source code
+      </a>
+      by
+      <em>Louisa Hesse</em>
+    </div>
+  );
+}
